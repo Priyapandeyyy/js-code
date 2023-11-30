52c8cb1af256fa48d4cc980307ba895b947d6bc6
Priya Pandey <priya.pandeyy321@gmail.com>
2023-12-01 01:58:48 +0530

    wip

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1443
+let accoutEmail  = "priya.pandeyy@gmail.com"
+var accountPassword = "2345"
+accountcity = "noida"
+let accountState;
+
+accountEmail = "haha@gmail.com"
+accountPassword = "7654"
+accountcity = "Delhi"
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accoutEmail, accountPassword, accountcity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+/* 
+There is a difference between old javascript and new javascript
+*/
+
+"use strict"; //treat all JS code as newer version. WE USUALLY DON'T HAVE TO WRITE USE STRICT.
+
+/*alert(3_+3)  This alert will not work as alert have slight different syntax now 
+We aew using nodejs not browser
+*/
+
+// Code readability should be high
