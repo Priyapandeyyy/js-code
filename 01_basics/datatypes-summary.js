5e12dcf28121489a4a2059cbd0de1ffc84584676
Priya Pandey <priya.pandeyy321@gmail.com>
2023-12-08 00:26:10 +0530

    commit message

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,65 @@
+//  Kis tarah se data ko memory mai rakha jata hai or access kiya jata hai use do types mai bata gaya hai. 
+// they are primitive and non-primitive.
+
+
+/*  PRIMITIVE DATATYPE
+7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+/*  REFERENCE (NON-PRIMITIVE)
+Array, Objects, Functions
+*/
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id === anotherId); // ==> false
+
+// const bigNumber = 72304829086474n // n lagane pe big int ban jata hai
+
+// const heros = ["shaktiman", "spiderman", 'baalveer'];
+// let myObj{                      
+//     name: Priya,
+//     age: 22,
+// }
+
+//curly braces ke andar object likhte hai.
+// isme datatype kuch bhi ho sakta hai
+
+
+//DECLARATION OF FUNCTION
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// Data ka type pata typeof se karte hai
+
+// console.log(typeof bigNumber); // ==> undefined
+// console.log(typeof outsideTemp); // ==> object
+// console.log(typeof myFunction);  // ==> Function
+// console.log(typeof symbol);  // ==> symbol
+
+
+
+
+// ******************************************************************************//
+
+// STACK AND HEAP MEMORY
+
+// Stack (Primitive), Heap (Non- Primitive)
+
+// In stack whatever we have created we will get a copy of it.
+// and in heap we get reference of orginal value, means changes will be done in original.
+
+
+let myYoutubename = "Priyapandey@com"
